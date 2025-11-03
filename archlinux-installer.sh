7ef01e3c06fb86de73357b81b864276835fd1abd
Terra88 <117510749+Terra88@users.noreply.github.com>
2025-11-03 09:55:05 +0200

    Update archlinux-installer.sh

--- archlinux-installer.sh
+++ archlinux-installer.sh
@@ -397,11 +397,35 @@ sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers || true
 systemctl enable NetworkManager
 systemctl enable sshd
 
-# 9) Install GRUB for UEFI
+# 9) Install GRUB for UEFI / BIOS
 # EFI partition is expected to be mounted on /boot (as done before chroot)
-echo "Installing GRUB (UEFI)..."
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --recheck
-grub-mkconfig -o /boot/grub/grub.cfg
+#echo "Installing GRUB (UEFI)..."
+#grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --recheck
+#grub-mkconfig -o /boot/grub/grub.cfg
+
+Install GRUB Bootloader
+# Check for UEFI or BIOS boot mode
+
+if [[ -d /sys/firmware/efi ]]; then
+  # UEFI Mode
+  echo "UEFI boot detected. Installing GRUB for UEFI..."
+  
+  # Ensure EFI partition is mounted at /mnt/boot
+  mkdir -p /mnt/boot
+  mount "$P1" /mnt/boot
+  
+  # Install GRUB for UEFI
+  arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --recheck
+  arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+  
+else
+  # BIOS Mode
+  echo "BIOS boot detected. Installing GRUB for BIOS..."
+  
+  # Install GRUB for BIOS
+  arch-chroot /mnt grub-install --target=i386-pc --recheck --bootloader-id=GRUB "$DEV"
+  arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+fi
 
 echo "Postinstall inside chroot finished."
 EOF
