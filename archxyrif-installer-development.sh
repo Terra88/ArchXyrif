a1952afae0f50110075c0d646b28ba2c8e934bf2
Tero.H <117510749+Terra88@users.noreply.github.com>
2025-11-21 12:26:13 +0200

    Update archxyrif-installer-development.sh

--- archxyrif-installer-development.sh
+++ archxyrif-installer-development.sh
@@ -1164,13 +1164,14 @@ window_manager() {
     WM_AUR_PKGS=()
     EXTRA_PKGS=()
     EXTRA_AUR_PKGS=()
+    WM_SERVICE=""
 
     # ---------- Set WM packages and selected WM ----------
     case "$WM_CHOICE" in
         1)
             SELECTED_WM="hyprland"
             echo -e "→ Selected: Hyprland"
-            WM_PKGS=(hyprland hyprpaper hyprshot hypridle hyprlock nano wget networkmanager network-manager-applet bluez bluez-utils blueman slurp swayidle swaylock waybar xdg-desktop-portal-hyprland qt5-wayland qt6-wayland qt5ct qt6ct xdg-utils breeze breeze-icons discover dolphin dolphin-plugins kate konsole krita kvantum polkit-kde-agent pipewire gst-plugin-pipewire pavucontrol gst-libav gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly otf-font-awesome ttf-hack cpupower brightnessctl thermald smartmontools htop btop nvtop qview ark kitty konsole firefox dunst wofi nwg-look nwg-displays archlinux-xdg-menu uwsm)
+            WM_PKGS=(hyprland hyprpaper hyprshot hypridle hyprlock nano wget networkmanager network-manager-applet bluez bluez-utils blueman slurp swayidle swaylock waybar xdg-desktop-portal-hyprland qt5-wayland qt6-wayland qt5ct qt6ct xdg-utils breeze breeze-icons discover dolphin dolphin-plugins kate konsole krita kvantum polkit-kde-agent pipewire gst-plugin-pipewire pavucontrol gst-libav gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly otf-font-awesome ttf-hack cpupower brightnessctl thermald smartmontools htop btop nvtop qview ark kitty konsole firefox dunst rofi wofi nwg-look nwg-displays archlinux-xdg-menu uwsm)
             WM_AUR_PKGS=(kvantum-theme-catppuccin-git qt6ct-kde wlogout wlrobs-hg)
             WM_SERVICE="bluetooth"
             ;;
@@ -1178,36 +1179,43 @@ window_manager() {
             SELECTED_WM="kde"
             echo -e "→ Selected: KDE Plasma"
             WM_PKGS=(plasma-desktop kde-applications konsole kate dolphin ark sddm firefox kitty)
+            WM_SERVICE=""
             ;;
         3)
             SELECTED_WM="gnome"
             echo -e "→ Selected: GNOME"
             WM_PKGS=(gnome gdm gnome-tweaks firefox kitty)
+            WM_SERVICE=""
             ;;
         4)
             SELECTED_WM="xfce"
             echo -e "→ Selected: XFCE"
             WM_PKGS=(xfce4 xfce4-goodies xarchiver gvfs pavucontrol lightdm-gtk-greeter firefox kitty)
+            WM_SERVICE=""
             ;;
         5)
             SELECTED_WM="niri"
             echo -e "→ Selected: Niri"
             WM_PKGS=(niri alacritty fuzzel mako swaybg swayidle swaylock waybar xdg-desktop-portal-gnome xorg-xwayland)
+            WM_SERVICE=""
             ;;
         6)
             SELECTED_WM="cinnamon"
             echo -e "→ Selected: Cinnamon"
             WM_PKGS=(cinnamon engrampa gnome-keyring gnome-screenshot gnome-terminal gvfs-smb system-config-printer xdg-user-dirs-gtk xed firefox kitty)
+            WM_SERVICE=""
             ;;
         7)
             SELECTED_WM="mate"
             echo -e "→ Selected: Mate"
             WM_PKGS=(mate mate-extra kitty firefox)
+            WM_SERVICE=""
             ;;
         8)
             SELECTED_WM="sway"
             echo -e "→ Selected: Sway"
             WM_PKGS=(sway swaybg swaylock swayidle waybar wofi xorg-xwayland wmenu slurp pavucontrol grim foot brightnessctl)
+            WM_SERVICE=""
             ;;
         9|*)
             SELECTED_WM="none"
@@ -1252,6 +1260,12 @@ window_manager() {
         safe_aur_install CHROOT_CMD[@] "${WM_AUR_PKGS[@]}"
     fi
 
+    # ---------- Enable WM service ----------
+    if [[ -n "$WM_SERVICE" ]]; then
+        "${CHROOT_CMD[@]}" systemctl enable "$WM_SERVICE"
+        echo -e "✅ Display manager service enabled: $WM_SERVICE"
+    fi
+    
     # ---------- Install extra packages ----------
     if [[ ${#EXTRA_PKGS[@]} -gt 0 ]]; then
         safe_pacman_install CHROOT_CMD[@] "${EXTRA_PKGS[@]}"
